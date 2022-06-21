5b63b785fa1e3125f792dc05f874ec1f59b6abeb
v0pe <dampblew01@gmail.com>
2022-06-21 09:22:17 -0400

    add price filter

---
+++ src/Components/Pages/FilterInput.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FilterPrice = ({ value, handlePrice }) => (
+  <div className="price-filter">
+    {' Filter the Price of Coin: '}
+    <input
+      type="number"
+      id="price-range"
+      name="price-range"
+      min="0"
+      max="9999999999"
+      value={value}
+      onChange={handlePrice}
+    />
+    {' USD($)'}
+  </div>
+);
+
+FilterPrice.propTypes = ({
+  value: PropTypes.number,
+  handlePrice: PropTypes.func,
+}).isRequired;
+
+export default FilterPrice;
--- src/Components/Pages/Homepage.js
+++ src/Components/Pages/Homepage.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LogoArea from '../HeroArea';
+import FilterPrice from './FilterInput';
 
 function Homepage() {
+  const [InputPrice, SetPriceInput] = useState(30);
+  const Pricechangehandler = ({ target }) => {
+    SetPriceInput(target.value);
+  };
+
   return (
     <section>
       <LogoArea logotext="Crypto" title="Major Crypto Markets" />
+      <FilterPrice value={InputPrice} handlePrice={Pricechangehandler} />
     </section>
   );
 }
