b67098951982f92646e245d387b0c55780b27a3f
v0pe <dampblew01@gmail.com>
2022-06-22 06:31:34 -0400

    routes rendered

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import './App.css';
 import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Homepage from './Components/Pages/Homepage';
-import { FetchCryptos } from './Redux/AllCrypto';
+import { FetchCryptos, selectCryptos } from './Redux/AllCrypto';
+import Crypto from './Components/Pages/Crypto';
 
 function App() {
   const dispatch = useDispatch();
+  const url = '/';
+
+  const cryptos = useSelector(selectCryptos);
 
   useEffect(() => {
     dispatch(FetchCryptos());
   }, [dispatch]);
 
+  const routes = cryptos.map((coin) => (
+    <Route key={coin.id} path={`${url}${coin.id}`} element={<Crypto />} />
+  ));
+
   return (
     <div>
       <Routes>
         <Route exact path="/" element={<Homepage />} />
+        { routes }
       </Routes>
 
     </div>
--- src/Components/Pages/Crypto.js
+++ src/Components/Pages/Crypto.js
@@ -5,6 +5,24 @@ function Crypto() {
   return (
     <section>
       <LogoArea logotext="Crypto" title="Bitcoin" />
+      <div className="detail-container">
+        <h2>Date</h2>
+        <p>
+          Source:
+          {' '}
+          the
+        </p>
+        <p>
+          Deaths until today:
+          {' '}
+          Thess
+        </p>
+        <p>
+          Deaths today:
+          {' '}
+          Deaths
+        </p>
+      </div>
     </section>
   );
 }
